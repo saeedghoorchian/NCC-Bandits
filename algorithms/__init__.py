bda162c90c07c10bcc2a88d0d97f313e5cbc998e
Evgenii Kortukov <elephunker1@gmail.com>
2022-05-24 17:24:56 +0200

    Remove undone parallel version of Alg1

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,5 +1,4 @@
 from algorithms.alg1 import Algorithm1
-from algorithms.alg1_parallel import Algorithm1Parallel
 from algorithms.egreedy import EpsilonGreedy
 from algorithms.linucb import LinUCB
 from algorithms.ps_linucb import PSLinUCB
